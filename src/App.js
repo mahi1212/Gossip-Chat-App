39f5789c2debf71a6264695aa1c7d1bf784d6717
mahinur05 <mahi121.mr2@gmail.com>
2023-03-25 00:23:59 +0600

    login Form added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { ChatEngine } from 'react-chat-engine'
-import './App.css'
-import ChatFeed from './components/ChatFeed'
+// 
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '3c9a291d-c163-4be1-aefe-95468df2df40';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="3c9a291d-c163-4be1-aefe-95468df2df40"
-      userName="joy"
-      userSecret="123456"
-      renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-  )
-}
+  );
+};
 
-export default App
+export default App;
